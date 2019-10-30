d56527359cac6ffac3e9510ae35b6d96ebc0d1c4
sangjun.lee1992 <sangjun.lee1992@gmail.com>
2019-10-31 00:07:31 +0900

    RB Tree 이론 내용 정리

--- SWMOOC_Open_Source_Data_Structure/SWMOOC_Open_Source_Data_Structure/4_1_Red_Black_Tree_Basic.cpp
+++ SWMOOC_Open_Source_Data_Structure/SWMOOC_Open_Source_Data_Structure/4_1_Red_Black_Tree_Basic.cpp
@@ -8,8 +8,27 @@
 #define VIDEO 0
 
 #if(VIDEO == 1)
-
-int main(void){
-}
+/*		No code here		*/
 #elif(VIDEO == 2)
+/*	RB Tree의 삽입 룰										*/
+/*	0. 밸런스는 root 노드로부터 모든 단말 노드로 가는 경로의		*/
+/*		검정 노드의 수가 같으면 밸런스가 맞는 것으로 간주		*/
+/*	1. root 노드는 언제나 검정								*/
+/*	2. 새로운 노드는 빨강										*/
+/*	3. 부모도 빨강, 삼촌도 빨강이면 color flip					*/
+/*		color flip :										*/
+/*			parent, uncle =>BLACK							*/
+/*			gparent => RED									*/
+/*	4. 부모가 빨강이면 회전 (gparent - parent - node)			*/
+/*		color flip :										*/
+/*			parent => BLACK									*/
+/*			gparent => RED									*/
+/*															*/
+/*		R-R 경사(Right-Right)인 경우:							*/
+/*			rotate_left(gparent);							*/
+
 #elif(VIDEO == 3)
+
+#elif(VIDEO == 4)
+
+#elif(VIDEO == 5)

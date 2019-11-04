58b790d6353cf313b38ae2d01202b52f528c4603
sangjun.lee1992 <sangjun.lee1992@gmail.com>
2019-11-04 22:48:09 +0900

    RB tree 강의 내용 정리 중 수정 필요

--- SWMOOC_Open_Source_Data_Structure/SWMOOC_Open_Source_Data_Structure/4_1_Red_Black_Tree_Basic.cpp
+++ SWMOOC_Open_Source_Data_Structure/SWMOOC_Open_Source_Data_Structure/4_1_Red_Black_Tree_Basic.cpp
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define VIDEO 0
+#define VIDEO 3
 
 #if(VIDEO == 1)
 /*		No code here		*/
@@ -28,6 +28,46 @@
 /*			rotate_left(gparent);							*/
 
 #elif(VIDEO == 3)
+typedef struct _node {
+	int sid;
+	struct rb_node node
+}SAWON;
+
+void my_insert(struct rb_root* root, SAWON* s) {
+	struct rb_node** p = &root->rb_node;
+	struct rb_node* parent = 0;
+
+	while (*p) {
+		SAWON* temp = rb_entry(*p, SAWON, node);
+		parent = *p;
+		if (parent->sid > temp->sid) {
+			p = &(*p)->left;
+		}
+		else if (parent->sid < temp->sid) {
+			p = &(*p)->right;
+		}
+		else {
+			return 0;
+		}
+		rb_link_node(&s->node, parent, p);
+		return l;
+	}
+}
+
+int main() {
+	struct rb_root root = { 0 };
+	int a[] = { 1,2,3,4,5,6,7,8 };
+	SAWON sawon[8];
+	display(&root);
+	for (int i = 0; i < 8; i++) {
+		sawon[i].sid = a[i];
+		my_insert(&root, sawon+i);
+		display(&root);
+	}
+
+	return 0;
+}
+
 
 #elif(VIDEO == 4)
 
